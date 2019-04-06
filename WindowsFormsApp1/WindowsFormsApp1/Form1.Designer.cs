be568412fc0452f15ff65342d907b4da80a066f0
Wojciech Oset <209415@edu.p.lodz.pl>
2019-04-06 12:15:08 +0200

    Lab2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 346);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Bitmap bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height, PixelFormat.Format32bppArgb); //tworzenie bitmapy
+
+            //Graphics ctx = Graphics.FromImage(bmp); //tworzenie kontekstu
+
+            /*
+            Matrix m = new Matrix();  
+            //m.Translate(-100, 100);  // przesuwanie o wektor
+            //m.Rotate(45.0f);         // obracanie obiektu
+            //m.Shear(2, 1);           // zakrzywienie
+            //m.Scale(2,2);            // skalowanie
+            ctx.Transform = m;
+
+            Rectangle rect = new Rectangle(10, 20, 200, 100); //rysowanie wypełnienia
+
+            ctx.FillRectangle(Brushes.Red, rect);
+
+            Pen p = new Pen(Color.Blue, 10.0f);
+
+            ctx.DrawRectangle(p, rect); //rysowanie prostokąta wokół wypełnienia
+
+            Font f = new Font(FontFamily.GenericSerif, 20.0f, FontStyle.Bold);
+
+            ctx.DrawString("Ala ma kota", f, Brushes.Black, 20, 10);
+            ctx.DrawString("Kot ma Ale", f, Brushes.Black, 40, 20);
+
+
+            bmp.Save("Obraz.png");
+
+            this.pictureBox1.Image = bmp;
+            
+
+            Bitmap bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height, PixelFormat.Format32bppArgb);
+            Graphics ctx = Graphics.FromImage(bmp);
+
+            Rectangle rect = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height);
+            ctx.FillRectangle(Brushes.Black, rect);
+
+            Font f = new Font(FontFamily.GenericSerif, 200.0f, FontStyle.Bold);
+            int posX = 0;
+            int posY = 0;
+
+            ctx.DrawString("Ala ma kota", f, Brushes.Red, posX, posY);
+
+
+            this.pictureBox1.Image = bmp;
+            */
+            
+        }
+
+        int posX = 0;
+        int pos2X = 0;
+        int posY = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Bitmap bmp = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height, PixelFormat.Format32bppArgb);
+            Graphics ctx = Graphics.FromImage(bmp);
+
+            Rectangle rect = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height);
+            Font f = new Font(FontFamily.GenericSerif, 200.0f, FontStyle.Bold);
+
+            ctx.FillRectangle(Brushes.Black, rect);
+
+            //Brush b = new SolidBrush(); - dodanie kanału alfa
+
+            ctx.DrawString("Ala ma kota", f, Brushes.Red, posX, posY);
+            ctx.DrawString("Kot ma Ale", f, Brushes.Red, pos2X, posY);
+
+            posX += 20;
+            pos2X -= 20;
+
+            //this.pictureBox1.Left += 20;
+
+            if (posX > this.ClientSize.Width || pos2X > this.ClientSize.Width)
+            {
+                posX =- this.ClientSize.Width;
+                pos2X = -this.ClientSize.Width;
+
+            }
+
+            this.pictureBox1.Image = bmp;
+        }
+    }
+}
